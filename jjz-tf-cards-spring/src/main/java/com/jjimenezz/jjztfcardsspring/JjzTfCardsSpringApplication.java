9780a02c9566d1f9b36b0ddb784407aae9c9d472
Jonathan MS <jimenezzj@hotmail.com>
2023-02-20 15:40:32 -0600

    Spring project created

---
+++ jjz-tf-cards-spring/src/main/java/com/jjimenezz/jjztfcardsspring/JjzTfCardsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jjimenezz.jjztfcardsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjzTfCardsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjzTfCardsSpringApplication.class, args);
+	}
+
+}
---
+++ jjz-tf-cards-spring/src/test/java/com/jjimenezz/jjztfcardsspring/JjzTfCardsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjimenezz.jjztfcardsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjzTfCardsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
