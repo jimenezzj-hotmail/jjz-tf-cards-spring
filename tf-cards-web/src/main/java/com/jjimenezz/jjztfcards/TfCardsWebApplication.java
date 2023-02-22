da9e8276068340f9533d8f6d9af7bc5462b95531
Jonathan MS <jimenezzj@hotmail.com>
2023-02-22 11:50:32 -0600

    web main classs fix-try 1

--- tf-cards-web/src/main/java/com/jjimenezz/jjztfcards/TfCardsWebApplication.java
+++ tf-cards-web/src/main/java/com/jjimenezz/jjztfcards/TfCardsWebApplication.java
@@ -1,4 +1,5 @@
 package com.jjimenezz.jjztfcards;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -6,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TfCardsWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TfCardsWebApplication.class, args);   
+        SpringApplication.run(TfCardsWebApplication.class, args);
     }
 
 }
