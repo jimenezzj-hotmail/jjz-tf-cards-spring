dbc499d778e6e032050c04517fffb14dfe1b28a5
Jonathan MS <jimenezzj@hotmail.com>
2023-02-22 11:40:14 -0600

    update spring web module main class

---
+++ tf-cards-web/src/main/java/com/jjimenezz/jjztfcards/TfCardsWebApplication.java
@@ -0,0 +1,12 @@
+package com.jjimenezz.jjztfcards;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfCardsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfCardsWebApplication.class, args);   
+    }
+
+}
